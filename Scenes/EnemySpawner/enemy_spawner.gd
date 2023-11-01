0d6300df08c1d553bb5464575aa50e4dbab5ec2c
JKSychta <83690016+JKSychta@users.noreply.github.com>
2023-11-01 13:06:01 +0100

    bugFix/EnemySpawn-01 Changed the way randomSpawnPoint() works in enemy_spawner.gd, fixing the issue with bad enemy placements. Now it simply adds a randomized Vector2 based off spawner's radius to the position of the spawner.

--- Scenes/EnemySpawner/enemy_spawner.gd
+++ Scenes/EnemySpawner/enemy_spawner.gd
@@ -4,31 +4,34 @@ signal everybody_dead
 
 @export var Enemy: PackedScene = preload("res://Scenes/Enemy/Bot_01/Enemy_bot1.tscn")
 @export var enemy_count: int = 5
+@export var respawn_time = 10
 @onready var timer = $RespawnTimer
 @onready var timer_label = $TimeDisplayLabel
 var alive_enemies_count: int
+@onready var area_radius = $Area2D/CollisionShape2D.shape.radius
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawn(enemy_count)
+	timer.wait_time = respawn_time
 	timer_label.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(get_child_count())
-
+	pass
+#	print(get_child_count())
 
 func spawn(amount = 1):
 	for i in range(amount):
 		var e = Enemy.instantiate()
 		add_child(e)
-		e.position = self.position + randomSpawnPoint()
+		e.position += Vector2(randomSpawnPoint(), randomSpawnPoint())
+		print(e.position)
 	alive_enemies_count = amount
 
 func randomSpawnPoint():
-	var angle := randi_range(0, PI)
-	var distance := randi_range(0, $Area2D/CollisionShape2D.shape.radius / 2)
-	return distance * Vector2.from_angle(angle)
+	var distance := randi_range(-area_radius, area_radius)	
+	return distance
 
 
 func _on_respawn_timer_timeout():
