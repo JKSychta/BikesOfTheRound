ee6d04c439328180445c3d4220deeed7156aa975
JKSychta <83690016+JKSychta@users.noreply.github.com>
2024-01-24 15:20:18 +0100

    Revert "dev-19-2"
    This reverts commit 4b4a0f5e8e8a29a29f0fd80b3865eff023a93b2d.

--- Scenes/Levels/Test Level/Delivery_System.gd
+++ Scenes/Levels/Test Level/Delivery_System.gd
@@ -2,6 +2,16 @@ extends Node
 
 signal navigationTarget(target)
 @onready var deliveryPoints= get_tree().get_nodes_in_group("DeliveryPoints")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	choose_destination()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
 
 func choose_destination():
 	var randomPoint = randi_range(0, len(deliveryPoints)-1)
