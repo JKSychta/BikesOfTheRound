c3c9fe769fc11f3a8982cd92b77e78654cb702e2
JKSychta <83690016+JKSychta@users.noreply.github.com>
2024-02-03 13:39:42 +0100

    Global.gd refactoring part2
    Some more small tweaks

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var score = 0
+var score: int = 0
 var loaded_scores: Dictionary
 var score_save_location: String = "user://scores.save"
 
@@ -8,17 +8,12 @@ var score_save_location: String = "user://scores.save"
 func _ready():
 	loaded_scores = load_score()
 
-func increase_score(points):
+func increase_score(points: int):
 	score+= points
 	
 func reset_score():
 	score = 0
 
-func create_save():
-	var score_to_save = {"scores":[1000,500,100]}
-	var score_file = FileAccess.open(score_save_location, FileAccess.WRITE)
-	var json_string = JSON.stringify(score_to_save)
-	score_file.store_line(json_string)
 
 func save_score():
 	var score_to_save = loaded_scores
@@ -28,11 +23,12 @@ func save_score():
 
 func load_score() -> Dictionary:
 	if not FileAccess.file_exists(score_save_location):
-		create_save()
+		loaded_scores = {"scores":[1000,500,100]}
+		save_score()
 	var score_file = FileAccess.open(score_save_location, FileAccess.READ)
 	var json_string = score_file.get_as_text()
-	var scoreDictionary = JSON.parse_string(json_string)
-	return scoreDictionary
+	var score_dictionary = JSON.parse_string(json_string)
+	return score_dictionary
 
 func update_score():
 	loaded_scores["scores"].sort()
